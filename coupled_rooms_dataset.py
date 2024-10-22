7935adb2d6181300c24d18918d307422dc1dade4
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2024-10-22 12:21:41 +0100

    Adjustable distance from walls

--- coupled_rooms_dataset.py
+++ coupled_rooms_dataset.py
@@ -15,12 +15,12 @@ class CommonSlopesRIR():
     rir: np.ndarray
     batch_id: Optional[int] = None
 
-def sample_room_interior(room_dim, n_locs):
+def sample_room_interior(room_dim, n_locs, lim=0.1):
     '''
-    sample coordinates of a point inside the room
+    sample coordinates of a point inside the room, with a minimum distance lim from the walls
     '''
-    x = np.random.uniform(0.25, room_dim[0]-0.25, size=(n_locs,1))
-    y = np.random.uniform(0.25, room_dim[1]-0.25, size=(n_locs,1))
-    z = np.random.uniform(0.25, room_dim[2]-0.25, size=(n_locs,1))
+    x = np.random.uniform(lim, room_dim[0]-lim, size=(n_locs,1))
+    y = np.random.uniform(lim, room_dim[1]-lim, size=(n_locs,1))
+    z = np.random.uniform(lim, room_dim[2]-lim, size=(n_locs,1))
     coord = np.hstack((x, y, z))
     return np.round(np.multiply(coord,1000))/1000  # round to 3 decimal places
