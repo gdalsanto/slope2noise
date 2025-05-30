fixed a bug in generate and made sure energy envelope calculation works for NDArray signals

slope2noise/generate.py
@@ -66,6 +66,7 @@ def shaped_wgn(
 
     # expand dimensions if necessary
     t_vals, a_vals, n_bands = slope_param_shape_check(t_vals, a_vals, f_bands)
+    print(t_vals.shape, a_vals.shape, n_bands)
 
     n_rirs, n_slopes = t_vals.shape[:2]
     time = np.linspace(0, (ir_len - 1) / fs, ir_len)  # time arrray
@@ -74,8 +75,6 @@ def shaped_wgn(
     gaussian_noise = np.zeros((n_rirs, ir_len, n_slopes + 1, n_bands))
     shaped_noise = np.zeros_like(gaussian_noise)
     rirs = np.zeros_like(gaussian_noise)
-    # generate random sequence of Gaussian noise
-    random_sequence = np.random.randn(n_rirs, ir_len, 1)
 
     # envelope is in linear scale, not quadratic, therefore decay rates halve,
     # and T values double

slope2noise/utils.py
@@ -169,28 +169,44 @@ def decay_kernel(
         return exponential
 
 
-def calculate_energy_envelope(sig: ArrayLike, fs: float,
-                              smooth_time_ms: float) -> ArrayLike:
+def calculate_energy_envelope(sig: NDArray,
+                              fs: float,
+                              smooth_time_ms: float,
+                              time_axis: int = -1) -> ArrayLike:
     """
     Calculate the energy envelope (broadband EDC) of a RIR
     Args:
-        sig (ArrayLike): 1D RIR signal
+        sig (ArrayLike): ND RIR signal
         fs (float): sampling rate
         smooth_time_ms (float): smoothing window length in ms,
                                 longer window leads to more smoothing
+        time_axis (int): axis along which time samples are specified
     """
     staps = ms_to_samps(smooth_time_ms / 2, fs)
     odd_win_len = 2 * staps - 1
     # normalised smoothing window
     bs = np.hanning(odd_win_len) / np.sum(np.hanning(odd_win_len))
-    # zero-pad signal on either side
-    padded_signal = np.concatenate((np.zeros(staps), sig**2, np.zeros(staps)),
-                                   axis=0)
-    # smooth signal by convolving with window
-    smoothed_signal = fftconvolve(bs, padded_signal)
-    env = np.real(np.sqrt(np.abs(smoothed_signal)))
-    # ignore the first win_len samples
-    env = env[odd_win_len + np.arange(len(sig))]
+
+    # Reshape bs to broadcast correctly along the time axis
+    bs_shape = [1] * sig.ndim
+    bs_shape[time_axis] = bs.shape[0]
+    bs_broadcasted = bs.reshape(bs_shape)
+
+    # Pad along the time axis, before and after the signal
+    pad_width = [(0, 0)] * sig.ndim
+    pad_width[time_axis] = (staps, staps)
+    padded_signal = np.pad(np.power(sig, 2), pad_width, mode='constant')
+
+    # Smooth using convolution along the time axis
+    smoothed_signal = fftconvolve(padded_signal,
+                                  bs_broadcasted,
+                                  mode='valid',
+                                  axes=time_axis)
+
+    # Take square root to get envelope
+    env = np.sqrt(smoothed_signal)
+    env = env[..., odd_win_len:]
+
     return env
 
 